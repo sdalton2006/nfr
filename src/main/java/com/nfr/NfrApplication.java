0f07afb4b4bba51b28bd009d48aa2f1eed62ac79
Simon Dalton <simon.andrew.dalton@gmail.com>
2026-07-24 10:01:06 +0100

    Initial project creation

---
+++ src/main/java/com/nfr/NfrApplication.java
@@ -0,0 +1,13 @@
+package com.nfr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NfrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nfr/NfrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nfr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
